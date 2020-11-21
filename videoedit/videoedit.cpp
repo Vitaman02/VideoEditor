9702391501b0c9e9bbb43ce326da913bb3350f9c
Vitaman02 <56309299+Vitaman02@users.noreply.github.com>
2020-11-21 07:19:53 +0200

    adding docs

--- videoedit/videoedit.cpp
+++ videoedit/videoedit.cpp
@@ -2,21 +2,55 @@
 #include <string>
 #include <opencv2/opencv.hpp>
 
+
 using namespace std;
 using namespace cv;
 
 
-int main() {
+string getVideoFilename() {
+    /// <summary>
+    /// Get the filename of the video to edit
+    /// </summary>
+    /// <returns></returns>
     string filename;
     cout << "What video do you want to edit: ";
     cin >> filename;
+
+    return filename;
+}
+
+
+int getOutVideoLength() {
+    /// <summary>
+    /// Get the length the output video should have
+    /// </summary>
+    /// <returns></returns>
+    int length;
+    cout << "How long should the output video be (in seconds): ";
+    cin >> length;
+
+    return length;
+}
+
+
+int main() {
+    // Get the filename of the video to edit
+    string filename = getVideoFilename();
+
+    // Get the length that the output video should be
+    int length = getOutVideoLength();
+    
+
+    // Open the video to edit
     VideoCapture cap(filename);
 
+    // If there was an error opening the video, show an error message
     if (!cap.isOpened()) {
         cout << "Error when opening video stream or file" << endl;
         return -1;
     }
 
+    // Iterate over each frame
     while (cap.isOpened()) {
         Mat frame;
         cap >> frame;
@@ -25,14 +59,19 @@ int main() {
             break;
         }
 
+
+        // Show the frame
         imshow("Frame", frame);
 
+
+        // Wait for the press of the 'esc' button for 25ms
         char c = (char)waitKey(25);
         if (c == 27) {
             break;
         }
     }
 
+    // Finally close the cap and close the window
     cap.release();
     destroyAllWindows();
 }
